a0d0501137745e400f2d3ce68a93466ba8f4a72d
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-12-22 00:46:02 +0100

    Delete utility directory

--- utility/MinecraftPlugin.java
+++
@@ -1,9 +0,0 @@
-package me.xcuzimsmart.utils;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public interface MinecraftPlugin {
-    Class<? extends JavaPlugin> instance();
-    String prefix();
-    String name();
-}
