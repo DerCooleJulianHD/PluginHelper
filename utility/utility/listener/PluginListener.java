d2628888bb3edd0396bf4b4967423d51f263bf28
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-12-22 02:14:18 +0100

    Add files via upload

---
+++ utility/utility/listener/PluginListener.java
@@ -0,0 +1,48 @@
+package me.xcuzimsmart.utils.listener;
+
+import me.xcuzimsmart.utils.*;
+
+import org.bukkit.Server;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.PluginManager;
+
+public abstract class PluginListener implements Listener {
+
+    private final MinecraftPlugin plugin;
+    private boolean enabled;
+
+    public PluginListener(MinecraftPlugin plugin) {
+        this.plugin = plugin;
+        this.setEnabled(false);
+    }
+
+
+    public void enable() {
+       final Server server = plugin.instance().getServer();
+       final PluginManager pluginManager = server.getPluginManager();
+       pluginManager.registerEvents(this, plugin.instance());
+       this.setEnabled(true);
+    }
+
+
+    public void disable() {
+        HandlerList.unregisterAll(this);
+        this.setEnabled(false);
+    }
+
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
+    private void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+	
+	
+	public MinecraftPlugin getMinecraftPlugin() {
+		return plugin;
+	}
+}
