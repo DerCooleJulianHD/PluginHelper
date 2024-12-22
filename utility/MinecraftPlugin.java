b1d259cce8ff0cc33894a78d3c2e86c888df750f
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2024-12-22 02:11:07 +0100

    Update MinecraftPlugin.java

--- utility/MinecraftPlugin.java
+++ utility/MinecraftPlugin.java
@@ -3,7 +3,7 @@ package me.xcuzimsmart.utils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public interface MinecraftPlugin {
-    Class<? extends JavaPlugin> instance();
+    JavaPlugin instance();
     String prefix();
     String name();
 }
