6cf809af2adec30cca06570df52656596f28c3c5
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-01-03 08:28:34 +0100

    Delete LibraryDevelopment/src/me/xcuzimsmart/utils/command directory

--- LibraryDevelopment/src/me/xcuzimsmart/utils/command/Syntax.java
+++
@@ -1,8 +0,0 @@
-package me.xcuzimsmart.utils.command;
-
-import java.util.List;
-
-public abstract class Syntax {
-    public abstract String getLabel();
-    public abstract List<String> getArguments();
-}
