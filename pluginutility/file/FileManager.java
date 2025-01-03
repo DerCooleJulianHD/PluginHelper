6d4f52ae5bda45c800e87c317d90340aafc0359f
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-01-03 09:08:07 +0100

    Add files via upload

---
+++ pluginutility/file/FileManager.java
@@ -0,0 +1,56 @@
+package pluginutility.file;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileManager {
+
+    // deletes a file and/or their directories
+    public static boolean delete(File file) {
+        // checking file is existing yet
+        if (!file.exists()) return true;
+
+        // ask for if the file we want to delete is a directory
+        if (file.isDirectory()) {
+            // here when file is a directory
+            final File[] childs = file.listFiles();
+
+            if (childs != null) {
+                // delete every file in this directory though
+                for (File child : childs) delete(child);
+            }
+        }
+
+        // deletes the overall file and return true when file isn't exist anymore
+        file.delete();
+        return file.exists();
+    }
+
+
+    // creates a file within directories
+    public static boolean createFile(File file) {
+        // asking if directory file is existing. when not it will be created.
+        if (!file.getParentFile().exists()) {
+            createDirectory(file.getParentFile()); // creating the parent file as the directory
+        }
+
+        try {
+            file.createNewFile(); // creating the storage file in the parent file.
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // checking if file is existing.
+        return file.exists();
+    }
+
+    public static File createDirectory(File file) {
+        // asking if file isn't a directory
+        if (!file.isDirectory()) {
+            throw new RuntimeException(file.getName() + " must be a directory file");
+        }
+
+        file.mkdirs(); // creates the directory file.
+        return file;
+    }
+}
