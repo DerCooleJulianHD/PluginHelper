abfdc265e409fdbf377e169b0dedece5e6e94001
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-01-03 08:29:41 +0100

    Delete LibraryDevelopment/src/me/xcuzimsmart/utils directory

--- LibraryDevelopment/src/me/xcuzimsmart/utils/MinecraftPlugin.java
+++
@@ -1,9 +0,0 @@
-package me.xcuzimsmart.utils;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public interface MinecraftPlugin {
-    JavaPlugin getPlugin();
-    String prefix();
-    String name();
-}
