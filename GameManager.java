e699fc11ff9c9bb43cd07934b69732fbd136ed39
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-08-30 18:10:30 +0200

    Add files via upload

---
+++ GameManager.java
@@ -0,0 +1,56 @@
+package de.xcuzimsmart.minigameapi.classes.game;
+
+import de.xcuzimsmart.minigameapi.classes.game.map.GameMap;
+import de.xcuzimsmart.minigameapi.classes.game.map.LocalGameMap;
+import de.xcuzimsmart.minigameapi.classes.game.map.OriginalGameMap;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class GameManager {
+
+    static File dir;
+    final String[] names;
+
+    public GameManager(File dir) {
+        GameManager.dir = dir;
+        if (!dir.exists()) dir.mkdirs();
+
+        this.names = dir.list();
+    }
+
+    public void loadGame(GameMap map) {
+        final Game game = new Game(map);
+    }
+
+    public void loadGame(int id) {
+        if (names.length != 0) {
+            if (id >= 0 && id < names.length || id == names.length) {
+                final String name = this.list().get(id);
+                if (name != null)
+                    loadGame(new OriginalGameMap(name));
+            }
+        }
+    }
+
+    public void loadRandomGame() {
+        final Random random = new Random();
+        final int id = random.nextInt(names.length);
+        this.loadGame(id);
+    }
+
+    public static File getDir() {
+        return dir;
+    }
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public List<String> list() {
+        return names == null ? new ArrayList<>() : Arrays.stream(names).toList();
+    }
+}
