ca6e9c035866c4bc4068ad73df1c33a781441ee0
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-09-01 19:58:37 +0200

    Update CommandInfo.java

--- CommandInfo.java
+++ CommandInfo.java
@@ -1,9 +1,3 @@
-package minecraft.lib.plugins.command;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -17,3 +11,4 @@ public @interface CommandInfo {
 
     String description() default "";
 }
+
