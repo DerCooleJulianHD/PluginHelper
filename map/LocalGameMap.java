6b24883d6c36a488f71d35ce2a6be5a1451cfe87
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-09-14 00:13:02 +0200

    Delete map directory

--- map/LocalGameMap.java
+++
@@ -1,8 +0,0 @@
-
-public final class LocalGameMap extends GameMap {
-
-    public LocalGameMap(File sourceFolder, String name) {
-        super(sourceFolder, new File(Bukkit.getWorldContainer().getParentFile(), name + "_active_" + new Date()), false);
-    }
-}
-
--- map/OriginalGameMap.java
+++
@@ -1,8 +0,0 @@
-
-public final class OriginalGameMap extends GameMap {
-
-    public OriginalGameMap(String name) {
-        super(new File(Bukkit.getWorldContainer().getParentFile(), name), new File(Bukkit.getWorldContainer().getParentFile(), name), true);
-    }
-}
-
