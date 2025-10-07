41d0f09f1e5ab710bba8dab2a1c38f90bdfd708b
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-10-07 23:01:56 +0200

    Aktualisieren von Prefixable.java

--- src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/Prefixable.java
+++ src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/Prefixable.java
@@ -4,7 +4,7 @@ public interface Prefixable {
 
     String getPrefix();
 
-    String getPrefixFromConfig(Configurator config);
+    String getPrefixFromConfig(Configurator config, String defaultValue);
 
     void setPrefix(String prefix);
 }
