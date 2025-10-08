2d0193784128d52afe51d1f75dc19cc34465f3e3
DerCooleJulianHD YT <109760232+DerCooleJulianHD@users.noreply.github.com>
2025-10-08 20:30:22 +0200

    Aktualisieren von MCSpigotPlugin.java

--- src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/MCSpigotPlugin.java
+++ src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/MCSpigotPlugin.java
@@ -59,6 +59,11 @@ public abstract class MCSpigotPlugin extends JavaPlugin implements Prefixable {
         this.prefix = prefix;
     }
 
+    @Override
+    public void setConfigPrefix(String prefix) {
+        config.writeString("prefix", prefix);
+    }
+
     public Scoreboard getScoreboard() {
         return scoreboard;
     }
