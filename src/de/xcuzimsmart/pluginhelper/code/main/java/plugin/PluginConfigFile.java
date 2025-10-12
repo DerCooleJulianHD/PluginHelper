779ce3fe6204e969d73e2266b949fe81d4702560
Julian <schrieverjulian@gmail.com>
2025-10-12 23:04:03 +0200

    Updating classes via IDE

--- src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/PluginConfigFile.java
+++ src/de/xcuzimsmart/pluginhelper/code/main/java/plugin/PluginConfigFile.java
@@ -1,8 +1,9 @@
 package de.xcuzimsmart.pluginhelper.code.main.java.plugin;
 
+import de.xcuzimsmart.pluginhelper.code.main.java.configuration.Defaultable;
 import de.xcuzimsmart.pluginhelper.code.main.java.configuration.YamlConfigurator;
 
-public class PluginConfigFile extends YamlConfigurator {
+public class PluginConfigFile extends YamlConfigurator implements Defaultable {
 
     public PluginConfigFile(MCSpigotPlugin plugin) {
         super(plugin, plugin.getDataFolder(), "config.yml");
