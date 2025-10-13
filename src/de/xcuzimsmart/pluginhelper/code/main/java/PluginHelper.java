d5fddeaa68f4197a3449aaf698ab78494499f123
Julian <schrieverjulian@gmail.com>
2025-10-13 21:11:30 +0200

    Update via IDE

--- src/de/xcuzimsmart/pluginhelper/code/main/java/PluginHelper.java
+++ src/de/xcuzimsmart/pluginhelper/code/main/java/PluginHelper.java
@@ -4,9 +4,17 @@ import de.xcuzimsmart.pluginhelper.code.main.java.plugin.MCSpigotPlugin;
 
 public class PluginHelper extends MCSpigotPlugin {
 
+    static PluginHelper instance;
+
     @Override
     public void onPluginEnable() {
+        instance = this;
+
         this.setPrefix(getPrefix());
         if (config.isEmpty()) this.setConfigPrefix(prefix);
+    }
+
+    public static PluginHelper getPluginHelper() {
+        return instance;
     }
 }
