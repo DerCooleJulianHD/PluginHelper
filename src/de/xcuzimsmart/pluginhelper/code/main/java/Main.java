84373c4d173d0a5d6f83eded3172f7aa5be9405a
Julian <schrieverjulian@gmail.com>
2025-10-13 10:58:04 +0200

    Updating classes via IDE

--- src/de/xcuzimsmart/pluginhelper/code/main/java/Main.java
+++ src/de/xcuzimsmart/pluginhelper/code/main/java/Main.java
@@ -1,22 +1,12 @@
 package de.xcuzimsmart.pluginhelper.code.main.java;
 
 import de.xcuzimsmart.pluginhelper.code.main.java.plugin.MCSpigotPlugin;
-import de.xcuzimsmart.pluginhelper.code.main.java.test.Test;
 
 public class Main extends MCSpigotPlugin {
 
-    private static Test test;
-
     @Override
     public void onPluginEnable() {
-        test = new Test();
-
         this.setPrefix(getPrefix());
         if (config.isEmpty()) this.setConfigPrefix(prefix);
     }
-
-    public static Test getTest() {
-        return test;
-    }
-
 }
